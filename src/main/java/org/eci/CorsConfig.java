a35dd5c56fe86ab5cfcc067aa37212d84cff296a
sebastian2929 <62757013+sebastian2929@users.noreply.github.com>
2024-11-09 14:07:34 -0500

    Update CorsConfig.java

--- src/main/java/org/eci/CorsConfig.java
+++ src/main/java/org/eci/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // Cambia esto por la URL de tu frontend si es diferente
+                .allowedOriginPatterns("https://intento100front-beh9fwa0ebbgcfff.canadacentral-01.azurewebsites.net/") // Cambia esto por la URL de tu frontend si es diferente
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
